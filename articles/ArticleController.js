32e3755cbf02879baeedeff0662645bad8af20d4
Elton Evaristo <elton.evaristo@gmail.com>
2021-03-31 01:03:05 -0300

    inclusão de homepage e view dos artigos

--- articles/ArticleController.js
+++ articles/ArticleController.js
@@ -5,7 +5,9 @@ const Article = require("./Article");
 const slugify = require("slugify");
 
 router.get("/admin/articles", (req, res) => {
-    Article.findAll().then(articles => {
+    Article.findAll({
+        include: [{model: Category}]
+    }).then(articles => {
         res.render("admin/articles/index",{articles: articles});
     })
 });
@@ -29,6 +31,24 @@ router.post("/articles/save", (req, res) => {
     }).then(() => {
         res.redirect("/admin/articles");
     });
+});
+
+router.post("/articles/delete", (req, res) => {
+    var id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){
+            Articles.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/artciles");
+            })
+        }else
+            res.redirect("/admin/articles");
+    }else {
+        res.redirect("/admin/articles");
+    }
 });
 
 module.exports = router;
