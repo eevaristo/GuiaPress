8acb96f7f40654baa00cd80ce560f1444ec53cea
Elton Evaristo <elton.evaristo@gmail.com>
2021-04-03 00:10:53 -0300

    Fim do projeto

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const session = require("express-session");
 const connection = require("./database/database");
 
 const categoriesController = require("./categories/CategoriesController");
@@ -12,6 +13,11 @@ const Category = require("./categories/Category")
 const User = require("./users/User")
 // View engine
 app.set('view engine', 'ejs');
+
+//Sessions
+app.use(session({
+    secret: "qualquercoisa", cookie: {maxAge: 30000}
+}))
 
 //Static
 app.use(express.static('public'));
@@ -35,6 +41,15 @@ connection
 app.use("/", categoriesController);
 app.use("/", articlesController);
 app.use("/", usersController)
+
+app.get("/session", (req,res)=>{
+    req.session.treinamento = "formação nodejs"
+    req.session.ano = 2019
+});
+
+app.get("/leitura", (req,res) =>{
+    treinamento: req.session.treinamento
+})
 
 app.get("/", (req, res) => {
 
--- users/UsersController.js
+++ users/UsersController.js
@@ -5,7 +5,9 @@ const bcrypt = require('bcryptjs');
 
 
 router.get("/admin/users", (req,res) =>{
-    res.send("listagem de usuários");
+    User.findAll().then(users =>{
+        res.render("admin/users/index",{users: users})
+    });
 });
 
 router.get("/admin/users/create", (req, res) =>{
@@ -16,18 +18,60 @@ router.post("/users/create", (req,res) =>{
     var email = req.body.email;
     var password = req.body.password;
 
+    User.findOne({where:{email: email}}).then( user => {
+        if(user == undefined){
+
+            User.create({
+                email: email,
+                password: hash
+            }).then(() =>{
+                res.redirect("/");
+            }).catch((err) =>{
+                res.redirect("/");
+            })
+
+        }else{
+            res.redirect("/admin/users/create")
+        }
+    })
+
     var salt = bcrypt.genSaltSync(10);
     var hash = bcrypt.hashSync(password, salt);
 
-    User.create({
-        email: email,
-        password: hash
-    }).then(() =>{
-        res.redirect("/");
-    }).catch((err) =>{
-        res.redirect("/");
-    })
 
+
+})
+
+router.get("/login", (req,res) =>{
+    res.render("admin/users/login");
+})
+
+router.post("/authenticate", (req, res) =>{
+    var email = req.body.email;
+    var password = req.body.password;
+
+    User.findOne({where: {email: email}}).then(user =>{
+        if(user != undefined){
+            var correct = bcrypt.compareSync(password,user.password);
+
+            if(correct){
+                req.session.user = {
+                    id: user.id,
+                    email: user.email
+                }
+                res.redirect("/admin/articles")
+            }else{
+                res.redirect("/login");
+            }
+        }else {
+            res.redirect("/login");
+        }
+    })
+})
+
+router.get("/logout" ,(req,res) =>{
+    req.session.user = undefined;
+    res.redirect("/");
 })
 
 module.exports = router;
